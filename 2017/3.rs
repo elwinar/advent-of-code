ef344ad4fab8b572508db6633a337e63a31b8039
Romain Baugue <romain.baugue@elwinar.com>
2017-12-03 16:47:51 +0100

    Day 3, Star 2

--- 2017/3.rs
+++ 2017/3.rs
@@ -10,10 +10,10 @@ fn main() {
     first_star(289326);
 
     second_star(1);
+    second_star(2);
     second_star(9);
     second_star(10);
-    second_star(12);
-    second_star(23);
+    second_star(25);
     second_star(1024);
     second_star(289326);
 }
@@ -22,7 +22,7 @@ fn first_star(input: i32) {
     println!("First star");
     println!("input={}", input);
 
-    let distance = ((input as f64 - 1.).sqrt() as i32 + 1) / 2;
+    let distance = ((input as f32 - 1.).sqrt() as i32 + 1) / 2;
     println!("distance={}", distance);
 
     let delta = input - (distance * 2 - 1).pow(2);
@@ -35,4 +35,68 @@ fn first_star(input: i32) {
     println!("manhattan={}", manhattan);
 
     println!();
+}
+
+fn second_star(input: i32) {
+    println!("Second star");
+    println!("input={}", input);
+
+    let size = ((input as f32).sqrt() as i32 / 2 * 2 + 5) as usize;
+    let mut sq = vec![vec![0; size]; size];
+
+    let mut i = size / 2;
+    let mut j = i;
+    let mut step = 2;
+    let mut w = 1;
+    sq[i][j] = w;
+    
+    'search:
+    loop {
+        i += 1;
+        j += 1;
+        for _ in 0..step {
+            j -= 1;
+            w = fill(&mut sq, i, j);
+            if w > input {
+                break 'search;
+            }
+        }
+
+        for _ in 0..step {
+            i -= 1;
+            w = fill(&mut sq, i, j);
+            if w > input {
+                break 'search;
+            }
+        }
+
+        for _ in 0..step {
+            j += 1;
+            w = fill(&mut sq, i, j);
+            if w > input {
+                break 'search;
+            }
+        }
+
+        for _ in 0..step {
+            i += 1;
+            w = fill(&mut sq, i, j);
+            if w > input {
+                break 'search;
+            }
+        }
+
+        step += 2;
+    }
+
+    println!("written={}", w);
+
+    println!();
+}
+
+fn fill(sq: &mut Vec<Vec<i32>>, i: usize, j: usize) -> i32 {
+    sq[i][j] = sq[i-1][j-1] + sq[i][j-1] + sq[i+1][j-1]
+             + sq[i-1][j]                + sq[i+1][j]
+             + sq[i-1][j+1] + sq[i][j+1] + sq[i+1][j+1];
+    return sq[i][j];
 }
