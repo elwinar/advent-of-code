8fb88491d7b805f4b9c5f54bebb3c86a7bbbcacc
Romain Baugue <romain.baugue@elwinar.com>
2019-12-13 01:33:26 +0100

    2019-10b

--- 2019/10b.cob
+++ 2019/10b.cob
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. AdventOfCode2019-10a.
+       PROGRAM-ID. AdventOfCode2019-10b.
 
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
@@ -17,29 +17,29 @@
        WORKING-STORAGE SECTION.
        01 InputPath PIC X(50).
        01 InputLen PIC 9(4).
+       01 InputPos PIC 9(4).
 
        01 Roids OCCURS 1 TO 9999 TIMES DEPENDING ON RoidsLen.
-        02 Ord PIC 9(2).
         02 Abs PIC 9(2).
+        02 Ord PIC 9(2).
+        02 Dist USAGE FLOAT-LONG.
+        02 Ang USAGE FLOAT-LONG.
        01 RoidsLen PIC 9(4).
 
        01 Idx PIC 9(4).
        01 Jdx PIC 9(4).
        01 Kdx PIC 9(4).
+       01 N PIC 9(4).
 
        01 cX USAGE FLOAT-LONG.
        01 cY USAGE FLOAT-LONG.
        01 cR USAGE FLOAT-LONG.
        01 cP USAGE FLOAT-LONG.
-       01 Angles USAGE FLOAT-LONG OCCURS 1 TO 9999 TIMES
-           DEPENDING ON RoidsLen.
-       01 LoS PIC 9(4).
-       01 Best PIC 9(4).
-       01 BestLoS PIC 9(4).
 
        PROCEDURE DIVISION.
        Begin.
            ACCEPT InputPath FROM ARGUMENT-VALUE.
+           ACCEPT InputPos FROM ARGUMENT-VALUE.
            OPEN INPUT InputFile.
 
            READ InputFile.
@@ -50,7 +50,6 @@
              IF Cell(Jdx) IS NOT EQUAL TO "#" 
               EXIT PERFORM CYCLE
              END-IF
-
              ADD 1 TO RoidsLen
              COMPUTE Abs(RoidsLen) = Jdx - 1
              COMPUTE Ord(RoidsLen) = Idx - 1
@@ -63,38 +62,38 @@
            CLOSE InputFile.
 
            PERFORM VARYING Idx FROM 1 UNTIL Idx > RoidsLen
-            MOVE 0 TO LoS
-
-            PERFORM VARYING Jdx FROM 1 UNTIL Jdx > RoidsLen
-             COMPUTE cX = Abs(Jdx) - Abs(Idx)
-             COMPUTE cY = Ord(Jdx) - Ord(Idx)
-             COMPUTE cR = FUNCTION SQRT(cX**2 + cY**2)
-             COMPUTE cP = (180 * FUNCTION ACOS(
-               cX / FUNCTION SQRT(cX**2 + cY**2)
-              )) / FUNCTION PI()
-             IF cY IS LESS THAN 0
-              COMPUTE cP = 360 - cP
-             END-IF
-
-             MOVE cP TO Angles(Jdx)
-             PERFORM VARYING Kdx FROM 1 UNTIL Kdx >= Jdx
-              IF Angles(Kdx) IS EQUAL TO cP
-               EXIT PERFORM
-              END-IF
-             END-PERFORM
-             IF Kdx IS EQUAL TO Jdx
-              ADD 1 TO LoS
-             END-IF
-
-            END-PERFORM
-
-            DISPLAY Idx " " Ord(Idx) "," Abs(Idx) " " LoS
-            IF LoS IS GREATER THAN BestLoS
-             MOVE Idx TO Best
-             MOVE LoS TO BestLoS
+            COMPUTE cX = Abs(Idx) - Abs(InputPos)
+            COMPUTE cY = Ord(Idx) - Ord(InputPos)
+            COMPUTE cR = FUNCTION SQRT(cX**2 + cY**2)
+            COMPUTE cP = (180 * FUNCTION ACOS(
+              cX / FUNCTION SQRT(cX**2 + cY**2)
+             )) / FUNCTION PI()
+            IF cY IS LESS THAN 0
+             COMPUTE cP = FUNCTION MOD(360 - cP, 360)
             END-IF
+            COMPUTE cP = FUNCTION MOD(360 + cP - 270, 360)
+            MOVE cP TO Ang(Idx)
+            MOVE cR TO Dist(Idx)
            END-PERFORM.
 
-           DISPLAY " "
-           DISPLAY Best " " Abs(Best) "," Ord(Best) " " BestLoS.
+           SORT Roids ON 
+            ASCENDING KEY Ang
+            ASCENDING KEY Dist.
 
+           MOVE Ang(1) TO cP
+           PERFORM VARYING Idx FROM 2 UNTIL Idx > RoidsLen
+            IF Ang(Idx) IS EQUAL TO cP
+             DISPLAY "Offsetting" " " Idx
+             COMPUTE Ang(Idx) = Ang(Idx - 1) + 360
+             EXIT PERFORM CYCLE
+            END-IF
+            MOVE Ang(Idx) TO cP
+           END-PERFORM.
+           SORT Roids ON 
+            ASCENDING KEY Ang
+            ASCENDING KEY Dist.
+
+           PERFORM VARYING Idx FROM 1 UNTIL Idx > RoidsLen
+            DISPLAY Idx " " Abs(Idx) "," Ord(Idx) " " Ang(Idx) ","
+            Dist(Idx)
+           END-PERFORM.
